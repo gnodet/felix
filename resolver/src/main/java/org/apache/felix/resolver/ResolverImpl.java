2ba3ab32926a9eacd1c0457628e21c96f8094822
Guillaume Nodet <gnodet@Guillaumes-MacBook-Pro.local>
2015-06-19 16:15:25 +0200

    Move toStringBlame into UseConstraintError Remove a few unused parameters

--- resolver/src/main/java/org/apache/felix/resolver/ResolverImpl.java
+++ resolver/src/main/java/org/apache/felix/resolver/ResolverImpl.java
@@ -575,7 +575,7 @@ public class ResolverImpl implements Resolver
                             allCandidates = session.getMultipleCardCandidates();
                         }
                         wireMap = populateDynamicWireMap(rc,
-                            host, dynamicReq, resourcePkgMap, wireMap, allCandidates);
+                            host, dynamicReq, wireMap, allCandidates);
                     }
                 }
                 finally
@@ -1847,9 +1847,8 @@ public class ResolverImpl implements Resolver
     }
 
     private static Map<Resource, List<Wire>> populateDynamicWireMap(
-        ResolveContext rc, Resource resource, Requirement dynReq,
-        Map<Resource, Packages> resourcePkgMap,
-        Map<Resource, List<Wire>> wireMap, Candidates allCandidates)
+            ResolveContext rc, Resource resource, Requirement dynReq,
+            Map<Resource, List<Wire>> wireMap, Candidates allCandidates)
     {
         wireMap.put(resource, Collections.<Wire>emptyList());
 
@@ -1912,145 +1911,6 @@ public class ResolverImpl implements Resolver
         {
             System.out.println("    " + entry.getKey() + " - " + entry.getValue().values());
         }
-    }
-
-    private static String toStringBlame(
-        ResolveContext rc, Candidates allCandidates, Blame blame)
-    {
-        StringBuilder sb = new StringBuilder();
-        if ((blame.m_reqs != null) && !blame.m_reqs.isEmpty())
-        {
-            for (int i = 0; i < blame.m_reqs.size(); i++)
-            {
-                Requirement req = blame.m_reqs.get(i);
-                sb.append("  ");
-                sb.append(Util.getSymbolicName(req.getResource()));
-                sb.append(" [");
-                sb.append(req.getResource().toString());
-                sb.append("]\n");
-                if (req.getNamespace().equals(PackageNamespace.PACKAGE_NAMESPACE))
-                {
-                    sb.append("    import: ");
-                }
-                else
-                {
-                    sb.append("    require: ");
-                }
-                sb.append(req.getDirectives().get(Namespace.REQUIREMENT_FILTER_DIRECTIVE));
-                sb.append("\n     |");
-                if (req.getNamespace().equals(PackageNamespace.PACKAGE_NAMESPACE))
-                {
-                    sb.append("\n    export: ");
-                }
-                else
-                {
-                    sb.append("\n    provide: ");
-                }
-                if ((i + 1) < blame.m_reqs.size())
-                {
-                    Capability cap = getSatisfyingCapability(
-                        rc,
-                        allCandidates,
-                        blame.m_reqs.get(i));
-                    if (cap.getNamespace().equals(PackageNamespace.PACKAGE_NAMESPACE))
-                    {
-                        sb.append(PackageNamespace.PACKAGE_NAMESPACE);
-                        sb.append("=");
-                        sb.append(cap.getAttributes()
-                            .get(PackageNamespace.PACKAGE_NAMESPACE).toString());
-                        Capability usedCap =
-                            getSatisfyingCapability(
-                                rc,
-                                allCandidates,
-                                blame.m_reqs.get(i + 1));
-                        sb.append("; uses:=");
-                        sb.append(usedCap.getAttributes()
-                            .get(PackageNamespace.PACKAGE_NAMESPACE));
-                    }
-                    else
-                    {
-                        sb.append(cap);
-                    }
-                    sb.append("\n");
-                }
-                else
-                {
-                    Capability export = getSatisfyingCapability(
-                        rc,
-                        allCandidates,
-                        blame.m_reqs.get(i));
-                    sb.append(export.getNamespace());
-                    sb.append(": ");
-                    Object namespaceVal = export.getAttributes().get(export.getNamespace());
-                    if (namespaceVal != null)
-                    {
-                        sb.append(namespaceVal.toString());
-                    }
-                    else
-                    {
-                        for (Entry<String, Object> attrEntry : export.getAttributes().entrySet())
-                        {
-                            sb.append(attrEntry.getKey()).append('=')
-                                .append(attrEntry.getValue()).append(';');
-                        }
-                    }
-                    if (export.getNamespace().equals(PackageNamespace.PACKAGE_NAMESPACE)
-                        && !export.getAttributes().get(PackageNamespace.PACKAGE_NAMESPACE)
-                        .equals(blame.m_cap.getAttributes().get(
-                                PackageNamespace.PACKAGE_NAMESPACE)))
-                    {
-                        sb.append("; uses:=");
-                        sb.append(blame.m_cap.getAttributes().get(PackageNamespace.PACKAGE_NAMESPACE));
-                        sb.append("\n    export: ");
-                        sb.append(PackageNamespace.PACKAGE_NAMESPACE);
-                        sb.append("=");
-                        sb.append(blame.m_cap.getAttributes()
-                            .get(PackageNamespace.PACKAGE_NAMESPACE).toString());
-                    }
-                    sb.append("\n  ");
-                    sb.append(Util.getSymbolicName(blame.m_cap.getResource()));
-                    sb.append(" [");
-                    sb.append(blame.m_cap.getResource().toString());
-                    sb.append("]");
-                }
-            }
-        }
-        else
-        {
-            sb.append(blame.m_cap.getResource().toString());
-        }
-        return sb.toString();
-    }
-
-    private static Capability getSatisfyingCapability(
-        ResolveContext rc, Candidates allCandidates, Requirement req)
-    {
-        // If the requiring revision is not resolved, then check in the
-        // candidate map for its matching candidate.
-        Capability cap = allCandidates.getFirstCandidate(req);
-        // Otherwise, if the requiring revision is resolved then check
-        // in its wires for the capability satisfying the requirement.
-        if (cap == null && rc.getWirings().containsKey(req.getResource()))
-        {
-            List<Wire> wires =
-                rc.getWirings().get(req.getResource()).getRequiredResourceWires(null);
-            req = getDeclaredRequirement(req);
-            for (Wire w : wires)
-            {
-                if (w.getRequirement().equals(req))
-                {
-// TODO: RESOLVER - This is not 100% correct, since requirements for
-//       dynamic imports with wildcards will reside on many wires and
-//       this code only finds the first one, not necessarily the correct
-//       one. This is only used for the diagnostic message, but it still
-//       could confuse the user.
-                    cap = w.getCapability();
-                    break;
-                }
-            }
-        }
-
-        return cap;
     }
 
     private static final class WireCandidate
@@ -2181,69 +2041,197 @@ public class ResolverImpl implements Resolver
 
     private static class UseConstraintError extends ResolutionError {
 
-        private final ResolveContext context;
-        private final Candidates allCandidates;
-        private final Resource resource;
-        private final String pkgName;
-        private final Blame blame1;
-        private final Blame blame2;
+        private final ResolveContext m_context;
+        private final Candidates m_allCandidates;
+        private final Resource m_resource;
+        private final String m_pkgName;
+        private final Blame m_blame1;
+        private final Blame m_blame2;
 
         public UseConstraintError(ResolveContext context, Candidates allCandidates, Resource resource, String pkgName, Blame blame) {
             this(context, allCandidates, resource, pkgName, blame, null);
         }
 
         public UseConstraintError(ResolveContext context, Candidates allCandidates, Resource resource, String pkgName, Blame blame1, Blame blame2) {
-            this.context = context;
-            this.allCandidates = allCandidates;
-            this.resource = resource;
-            this.pkgName = pkgName;
-            this.blame1 = blame1;
-            this.blame2 = blame2;
+            this.m_context = context;
+            this.m_allCandidates = allCandidates;
+            this.m_resource = resource;
+            this.m_pkgName = pkgName;
+            this.m_blame1 = blame1;
+            this.m_blame2 = blame2;
         }
 
         public String getMessage() {
-            if (blame2 == null)
+            if (m_blame2 == null)
             {
                 return "Uses constraint violation. Unable to resolve resource "
-                        + Util.getSymbolicName(resource)
-                        + " [" + resource
+                        + Util.getSymbolicName(m_resource)
+                        + " [" + m_resource
                         + "] because it exports package '"
-                        + pkgName
+                        + m_pkgName
                         + "' and is also exposed to it from resource "
-                        + Util.getSymbolicName(blame1.m_cap.getResource())
-                        + " [" + blame1.m_cap.getResource()
+                        + Util.getSymbolicName(m_blame1.m_cap.getResource())
+                        + " [" + m_blame1.m_cap.getResource()
                         + "] via the following dependency chain:\n\n"
-                        + toStringBlame(context, allCandidates, blame1);
+                        + toStringBlame(m_blame1);
             }
             else
             {
                 return  "Uses constraint violation. Unable to resolve resource "
-                        + Util.getSymbolicName(resource)
-                        + " [" + resource
+                        + Util.getSymbolicName(m_resource)
+                        + " [" + m_resource
                         + "] because it is exposed to package '"
-                        + pkgName
+                        + m_pkgName
                         + "' from resources "
-                        + Util.getSymbolicName(blame1.m_cap.getResource())
-                        + " [" + blame1.m_cap.getResource()
+                        + Util.getSymbolicName(m_blame1.m_cap.getResource())
+                        + " [" + m_blame1.m_cap.getResource()
                         + "] and "
-                        + Util.getSymbolicName(blame2.m_cap.getResource())
-                        + " [" + blame2.m_cap.getResource()
+                        + Util.getSymbolicName(m_blame2.m_cap.getResource())
+                        + " [" + m_blame2.m_cap.getResource()
                         + "] via two dependency chains.\n\nChain 1:\n"
-                        + toStringBlame(context, allCandidates, blame1)
+                        + toStringBlame(m_blame1)
                         + "\n\nChain 2:\n"
-                        + toStringBlame(context, allCandidates, blame2);
+                        + toStringBlame(m_blame2);
             }
         }
 
         public Collection<Requirement> getUnresolvedRequirements() {
-            if (blame2 == null)
+            if (m_blame2 == null)
             {
                 return Collections.emptyList();
             }
             else
             {
-                return Collections.singleton(blame2.m_reqs.get(0));
+                return Collections.singleton(m_blame2.m_reqs.get(0));
             }
+        }
+
+        private String toStringBlame(Blame blame)
+        {
+            StringBuilder sb = new StringBuilder();
+            if ((blame.m_reqs != null) && !blame.m_reqs.isEmpty())
+            {
+                for (int i = 0; i < blame.m_reqs.size(); i++)
+                {
+                    Requirement req = blame.m_reqs.get(i);
+                    sb.append("  ");
+                    sb.append(Util.getSymbolicName(req.getResource()));
+                    sb.append(" [");
+                    sb.append(req.getResource().toString());
+                    sb.append("]\n");
+                    if (req.getNamespace().equals(PackageNamespace.PACKAGE_NAMESPACE))
+                    {
+                        sb.append("    import: ");
+                    }
+                    else
+                    {
+                        sb.append("    require: ");
+                    }
+                    sb.append(req.getDirectives().get(Namespace.REQUIREMENT_FILTER_DIRECTIVE));
+                    sb.append("\n     |");
+                    if (req.getNamespace().equals(PackageNamespace.PACKAGE_NAMESPACE))
+                    {
+                        sb.append("\n    export: ");
+                    }
+                    else
+                    {
+                        sb.append("\n    provide: ");
+                    }
+                    if ((i + 1) < blame.m_reqs.size())
+                    {
+                        Capability cap = getSatisfyingCapability(blame.m_reqs.get(i));
+                        if (cap.getNamespace().equals(PackageNamespace.PACKAGE_NAMESPACE))
+                        {
+                            sb.append(PackageNamespace.PACKAGE_NAMESPACE);
+                            sb.append("=");
+                            sb.append(cap.getAttributes()
+                                    .get(PackageNamespace.PACKAGE_NAMESPACE).toString());
+                            Capability usedCap =
+                                    getSatisfyingCapability(blame.m_reqs.get(i + 1));
+                            sb.append("; uses:=");
+                            sb.append(usedCap.getAttributes()
+                                    .get(PackageNamespace.PACKAGE_NAMESPACE));
+                        }
+                        else
+                        {
+                            sb.append(cap);
+                        }
+                        sb.append("\n");
+                    }
+                    else
+                    {
+                        Capability export = getSatisfyingCapability(blame.m_reqs.get(i));
+                        sb.append(export.getNamespace());
+                        sb.append(": ");
+                        Object namespaceVal = export.getAttributes().get(export.getNamespace());
+                        if (namespaceVal != null)
+                        {
+                            sb.append(namespaceVal.toString());
+                        }
+                        else
+                        {
+                            for (Entry<String, Object> attrEntry : export.getAttributes().entrySet())
+                            {
+                                sb.append(attrEntry.getKey()).append('=')
+                                        .append(attrEntry.getValue()).append(';');
+                            }
+                        }
+                        if (export.getNamespace().equals(PackageNamespace.PACKAGE_NAMESPACE)
+                                && !export.getAttributes().get(PackageNamespace.PACKAGE_NAMESPACE)
+                                .equals(blame.m_cap.getAttributes().get(
+                                        PackageNamespace.PACKAGE_NAMESPACE)))
+                        {
+                            sb.append("; uses:=");
+                            sb.append(blame.m_cap.getAttributes().get(PackageNamespace.PACKAGE_NAMESPACE));
+                            sb.append("\n    export: ");
+                            sb.append(PackageNamespace.PACKAGE_NAMESPACE);
+                            sb.append("=");
+                            sb.append(blame.m_cap.getAttributes()
+                                    .get(PackageNamespace.PACKAGE_NAMESPACE).toString());
+                        }
+                        sb.append("\n  ");
+                        sb.append(Util.getSymbolicName(blame.m_cap.getResource()));
+                        sb.append(" [");
+                        sb.append(blame.m_cap.getResource().toString());
+                        sb.append("]");
+                    }
+                }
+            }
+            else
+            {
+                sb.append(blame.m_cap.getResource().toString());
+            }
+            return sb.toString();
+        }
+
+        private Capability getSatisfyingCapability(Requirement req)
+        {
+            // If the requiring revision is not resolved, then check in the
+            // candidate map for its matching candidate.
+            Capability cap = m_allCandidates.getFirstCandidate(req);
+            // Otherwise, if the requiring revision is resolved then check
+            // in its wires for the capability satisfying the requirement.
+            if (cap == null && m_context.getWirings().containsKey(req.getResource()))
+            {
+                List<Wire> wires =
+                        m_context.getWirings().get(req.getResource()).getRequiredResourceWires(null);
+                req = getDeclaredRequirement(req);
+                for (Wire w : wires)
+                {
+                    if (w.getRequirement().equals(req))
+                    {
+                        // TODO: RESOLVER - This is not 100% correct, since requirements for
+                        //       dynamic imports with wildcards will reside on many wires and
+                        //       this code only finds the first one, not necessarily the correct
+                        //       one. This is only used for the diagnostic message, but it still
+                        //       could confuse the user.
+                        cap = w.getCapability();
+                        break;
+                    }
+                }
+            }
+
+            return cap;
         }
 
     }
